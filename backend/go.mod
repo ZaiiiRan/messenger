d6df102bb079e764ee887eb955517a6277bea363
ZaiiiRan <a1b075753@gmail.com>
2024-10-31 17:25:28 +0300

    getting user from db, hash password

--- backend/go.mod
+++ backend/go.mod
@@ -9,6 +9,8 @@ require (
 
 require github.com/golang-jwt/jwt/v5 v5.2.1
 
+require golang.org/x/crypto v0.28.0
+
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/google/uuid v1.5.0 // indirect
@@ -21,5 +23,5 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
--- backend/internal/models/user/user.go
+++ backend/internal/models/user/user.go
@@ -1,7 +1,10 @@
 package user
 
 import (
+	pgDB "backend/internal/dbs/pgDB"
+	"database/sql"
 	"time"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
@@ -19,3 +22,74 @@ type User struct {
 	CreatedAt   time.Time  `json:"created_at"`
 }
 
+// Get user by id
+func GetUserByID(ID uint64) (*User, error) {
+	db := pgDB.GetDB()
+	row := db.QueryRow(`SELECT * FROM users WHERE id = $1`, ID)
+	user, err := createUserFromSQLRow(row)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// Get user by username
+func GetUserByUsername(username string) (*User, error) {
+	db := pgDB.GetDB()
+	row := db.QueryRow(`SELECT * FROM users WHERE username = $1`, username)
+	user, err := createUserFromSQLRow(row)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// Get user by email
+func GetUserByEmail(email string) (*User, error) {
+	db := pgDB.GetDB()
+	row := db.QueryRow(`SELECT * FROM users WHERE email = $1`, email)
+	user, err := createUserFromSQLRow(row)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// Get user by phone
+func GetUserByPhone(phone string) (*User, error) {
+	db := pgDB.GetDB()
+	row := db.QueryRow(`SELECT * FROM users WHERE phone = $1`, phone)
+	user, err := createUserFromSQLRow(row)
+	if err == sql.ErrNoRows {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+// parsing user from sql row
+func createUserFromSQLRow(row *sql.Row) (*User, error) {
+	var user User
+	err := row.Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.Phone, 
+		&user.Firstname, &user.Lastname, &user.Birthdate,
+		&user.IsDeleted, &user.IsBanned, &user.IsActivated, &user.CreatedAt)
+	return &user, err
+}
+
+// hashing password
+func hashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), 6)
+	return string(hash), err
+}
+
+// compare passwords
+func comparePasswords(hashedPassword, password string) bool {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password)) == nil
+}
