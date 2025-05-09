11b6e6557a208d74a1fe638bca3ece6413f91c00
ZaiiiRan <a1b075753@gmail.com>
2025-05-10 00:15:34 +0300

    frontend: fix app index

--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -3,3 +3,6 @@ import en from './locales/en.json'
 import ru from './locales/ru.json'
 i18n.addResourceBundle('en', 'app', en)
 i18n.addResourceBundle('ru', 'app', ru)
+
+import App from './App'
+export { App }
