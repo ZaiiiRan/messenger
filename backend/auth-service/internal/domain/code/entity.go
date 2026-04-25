4a17ea7ca6e6a95213609180f08904ffad8db3c6
ZaiiiRan <a1b075753@gmail.com>
2026-04-26 00:25:02 +0300

    feat(auth-service): code domain

---
+++ backend/auth-service/internal/domain/code/entity.go
@@ -0,0 +1,97 @@
+package code
+
+import (
+	"time"
+)
+
+const (
+	maxGenerationsLeft = 3
+	codeTTL            = 10 * time.Minute
+)
+
+type Code struct {
+	id              int64
+	userId          string
+	code            string
+	generationsLeft int
+	expiresAt       time.Time
+	createdAt       time.Time
+	updatedAt       time.Time
+}
+
+func New(userId string) (*Code, error) {
+	c := &Code{}
+	c.generationsLeft = maxGenerationsLeft
+	c.userId = userId
+
+	if err := c.GenerateCode(); err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	c.createdAt = now
+	c.updatedAt = now
+
+	return c, nil
+}
+
+func FromStorage(
+	id int64,
+	userId, code string,
+	generationsLeft int,
+	expiresAt, createdAt, updatedAt time.Time,
+) *Code {
+	return &Code{
+		id:              id,
+		userId:          userId,
+		code:            code,
+		generationsLeft: generationsLeft,
+		expiresAt:       expiresAt,
+		createdAt:       createdAt,
+		updatedAt:       updatedAt,
+	}
+}
+
+func (c *Code) GetID() int64            { return c.id }
+func (c *Code) GetUserID() string       { return c.userId }
+func (c *Code) GetCode() string         { return c.code }
+func (c *Code) GetGenerationsLeft() int { return c.generationsLeft }
+func (c *Code) GetExpiresAt() time.Time { return c.expiresAt }
+func (c *Code) GetCreatedAt() time.Time { return c.createdAt }
+func (c *Code) GetUpdatedAt() time.Time { return c.updatedAt }
+
+func (c *Code) SetID(id int64) {
+	if c.id == 0 {
+		c.id = id
+	}
+}
+
+func (c *Code) GenerateCode() error {
+	if c.GetID() == 0 {
+		if c.generationsLeft <= 0 && time.Since(c.updatedAt) < 5*time.Minute {
+			return NewCodeValidationError("the number of code resends has been exhausted")
+		} else if c.generationsLeft <= 0 {
+			c.generationsLeft = maxGenerationsLeft
+		}
+	}
+	c.generationsLeft--
+
+	code, err := generateSixDigitCode()
+	if err != nil {
+		return err
+	}
+	c.code = code
+	c.expiresAt = time.Now().Add(codeTTL)
+	c.updatedAt = time.Now()
+	return nil
+}
+
+func (c *Code) CheckCode(rawCode string) (bool, error) {
+	if time.Now().After(c.expiresAt) {
+		return false, NewCodeValidationError("code has been expired")
+	}
+	if c.code == rawCode {
+		return true, nil
+	}
+	return false, nil
+}
