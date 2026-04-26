5e706026155ceb3563aafab29c6fd1919e6af205
ZaiiiRan <a1b075753@gmail.com>
2026-04-26 03:08:48 +0300

    feat(auth-service): sanitize request functions

--- backend/auth-service/internal/transport/server/grpc/handler.go
+++ backend/auth-service/internal/transport/server/grpc/handler.go
@@ -1,8 +1,13 @@
 package grpcserver
 
 import (
+	"context"
+
 	pb "github.com/ZaiiiRan/messenger/backend/auth-service/gen/go/auth/v1"
 	authservice "github.com/ZaiiiRan/messenger/backend/auth-service/internal/services/auth"
+	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/utils"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type authHandler struct {
@@ -14,4 +19,36 @@ func newAuthHandler(authService authservice.AuthService) *authHandler {
 	return &authHandler{
 		authService: authService,
 	}
+}
+
+func (h *authHandler) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	utils.SanitizeRegisterRequest(req)
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) GetNewConfirmationCode(ctx context.Context, req *pb.GetNewConfirmationCodeRequest) (*pb.GetNewConfirmationCodeResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) Confirm(ctx context.Context, req *pb.ConfirmRequest) (*pb.ConfirmResponse, error) {
+	utils.SanitizeConfirmRequest(req)
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	utils.SanitizeLoginRequest(req)
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) Refresh(ctx context.Context, req *pb.RefreshRequest) (*pb.RefreshResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) Logout(ctx context.Context, req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
+}
+
+func (h *authHandler) ChangePassword(ctx context.Context, req *pb.ChangePasswordRequest) (*pb.ChangePasswordResponse, error) {
+	utils.SanitizeChangePasswordRequest(req)
+	return nil, status.Errorf(codes.Unimplemented, "unimplemented")
 }
