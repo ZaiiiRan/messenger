5cab5c1bd49a71c0287a2e143f7d3a41bc2d6924
ZaiiiRan <a1b075753@gmail.com>
2026-04-26 04:10:33 +0300

    feat(auth-service): password service

--- backend/auth-service/internal/services/password/password_data_provider.go
+++ backend/auth-service/internal/services/password/password_data_provider.go
@@ -1,6 +1,12 @@
 package passwordservice
 
 import (
+	"context"
+
+	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/domain/password"
+	postgresimpl "github.com/ZaiiiRan/messenger/backend/auth-service/internal/repositories/impl/postgres"
+	redisimpl "github.com/ZaiiiRan/messenger/backend/auth-service/internal/repositories/impl/redis"
+	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/repositories/models"
 	uow "github.com/ZaiiiRan/messenger/backend/auth-service/internal/repositories/unitofwork/postgres"
 	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/transport/postgres"
 	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/transport/redis"
@@ -20,4 +26,54 @@ func newPasswordDataProvider(pg *postgres.PostgresClient, redis *redis.RedisClie
 
 func (pdp *passwordDataProvider) newUOW() *uow.UnitOfWork {
 	return uow.New(pdp.pg)
+}
+
+func (pdp *passwordDataProvider) getByUserID(ctx context.Context, userID string, uow *uow.UnitOfWork) (*password.Password, error) {
+	cacheRepo := redisimpl.NewPasswordCacheRepository(pdp.redis)
+	password, err := cacheRepo.GetPasswordByUserId(ctx, userID)
+	if err == nil && password != nil {
+		return password, nil
+	}
+
+	pgConn, err := uow.GetConn(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	dbRepo := postgresimpl.NewPasswordRepository(pgConn)
+	query := models.NewQueryPasswordDal(nil, &userID)
+	password, err = dbRepo.QueryPassword(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	if password == nil {
+		return nil, nil
+	}
+
+	cacheRepo.SetPasswordByUserId(ctx, password)
+	return password, nil
+}
+
+func (pdp *passwordDataProvider) save(ctx context.Context, p *password.Password, uow *uow.UnitOfWork) error {
+	pgConn, err := uow.GetConn(ctx)
+	if err != nil {
+		return err
+	}
+
+	dbRepo := postgresimpl.NewPasswordRepository(pgConn)
+
+	if p.GetID() == 0 {
+		if err := dbRepo.CreatePassword(ctx, p); err != nil {
+			return err
+		}
+	} else {
+		if err := dbRepo.UpdatePassword(ctx, p); err != nil {
+			return err
+		}
+	}
+
+	cacheRepo := redisimpl.NewPasswordCacheRepository(pdp.redis)
+	cacheRepo.SetPasswordByUserId(ctx, p)
+
+	return nil
 }
--- backend/auth-service/internal/services/password/password_service.go
+++ backend/auth-service/internal/services/password/password_service.go
@@ -1,12 +1,23 @@
 package passwordservice
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
+	userpb "github.com/ZaiiiRan/messenger/backend/auth-service/gen/go/user/v1"
+	passworddomain "github.com/ZaiiiRan/messenger/backend/auth-service/internal/domain/password"
+	uow "github.com/ZaiiiRan/messenger/backend/auth-service/internal/repositories/unitofwork/postgres"
 	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/transport/postgres"
 	"github.com/ZaiiiRan/messenger/backend/auth-service/internal/transport/redis"
+	"github.com/ZaiiiRan/messenger/backend/go-common/pkg/ctxmetadata"
 	"go.uber.org/zap"
 )
 
 type PasswordService interface {
+	CreatePassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (*passworddomain.Password, error)
+	CheckPassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (bool, error)
+	UpdatePassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (*passworddomain.Password, error)
 }
 
 type passwordService struct {
@@ -19,4 +30,86 @@ func New(pgClient *postgres.PostgresClient, redisClient *redis.RedisClient, log 
 		passwordDataProvider: newPasswordDataProvider(pgClient, redisClient),
 		log:                  log,
 	}
+}
+
+func (s *passwordService) CreatePassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (*passworddomain.Password, error) {
+	l := s.log.With("op", "create_password", "req_id", ctxmetadata.GetReqIdFromContext(ctx))
+
+	p, err := passworddomain.New(user.Id, rawPassword)
+	if err != nil {
+		var valErr *passworddomain.PasswordValidationError
+		if errors.As(err, &valErr) {
+			l.Warnw("password.create_password_failed.validation_error", "err", err)
+		} else {
+			l.Warnw("password.create_password_failed.validation_error", "err", err)
+		}
+		return nil, err
+	}
+
+	existed, err := s.passwordDataProvider.getByUserID(ctx, user.Id, uow)
+	if err != nil {
+		l.Errorw("password.create_password_failed", "err", err)
+		return nil, err
+	}
+	if existed != nil {
+		return p, nil
+	}
+
+	if err := s.passwordDataProvider.save(ctx, p, uow); err != nil {
+		l.Errorw("password.create_password_failed", "err", err)
+		return nil, err
+	}
+
+	l.Infow("password.create_password.success")
+	return p, nil
+}
+
+func (s *passwordService) CheckPassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (bool, error) {
+	l := s.log.With("op", "check_password", "req_id", ctxmetadata.GetReqIdFromContext(ctx))
+
+	p, err := s.passwordDataProvider.getByUserID(ctx, user.Id, uow)
+	if err != nil {
+		l.Errorw("password.check_password_failed", "err", err)
+		return false, err
+	}
+	if p == nil {
+		l.Errorw("password.check_password_failed", "err", "password not found")
+		return false, fmt.Errorf("password not found")
+	}
+
+	correct := p.CheckPassword(rawPassword)
+	l.Infow("password.check_password.success")
+	return correct, nil
+}
+
+func (s *passwordService) UpdatePassword(ctx context.Context, uow *uow.UnitOfWork, user *userpb.User, rawPassword string) (*passworddomain.Password, error) {
+	l := s.log.With("op", "update_employer_password", "req_id", ctxmetadata.GetReqIdFromContext(ctx))
+
+	p, err := s.passwordDataProvider.getByUserID(ctx, user.Id, uow)
+	if err != nil {
+		l.Errorw("password.update_password_failed", "err", err)
+		return nil, err
+	}
+	if p == nil {
+		l.Errorw("password.update_password_failed", "err", "password not found")
+		return nil, fmt.Errorf("password not found")
+	}
+
+	if err := p.SetPassword(rawPassword); err != nil {
+		var valErr *passworddomain.PasswordValidationError
+		if errors.As(err, &valErr) {
+			l.Warnw("password.update_password_failed.validation_error", "err", err)
+		} else {
+			l.Errorw("password.update_password_failed", "err", err)
+		}
+		return nil, err
+	}
+
+	if err := s.passwordDataProvider.save(ctx, p, uow); err != nil {
+		l.Errorw("password.update_password_failed", "err", err)
+		return nil, err
+	}
+
+	l.Infow("password.update_password.success")
+	return p, nil
 }
