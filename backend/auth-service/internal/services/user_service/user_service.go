34b69c1f37692b2916a5ca4d306f9aba3ab0899b
ZaiiiRan <a1b075753@gmail.com>
2026-05-17 23:50:18 +0300

    feat(auth-service): update pb and change logs in user service

--- backend/auth-service/internal/services/user_service/user_service.go
+++ backend/auth-service/internal/services/user_service/user_service.go
@@ -173,7 +173,7 @@ func (s *service) GetUsers(ctx context.Context, req *pb.GetUsersRequest) ([]*pb.
 		if status.Code(err) == codes.InvalidArgument {
 			l.Warnw("user.get_users_failed.invalid_argument", "err", err)
 		} else {
-			l.Errorw("user.get_users_failed.get_user_by_email_error", "err", err)
+			l.Errorw("user.get_users_failed", "err", err)
 		}
 		return nil, err
 	}
